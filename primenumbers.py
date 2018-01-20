66cdc4b04ce30b131e798518e4da7bbfca5f6987
Peter Mancuso <pm@electrical.jp>
2018-01-19 21:28:16 -0500

    init

---
+++ primenumbers.py
@@ -0,0 +1,19 @@
+import decorators
+
+
+@decorators.timeit
+def primes_sieve2(limit):
+    a = [True] * limit
+    a[0] = a[1] = False
+
+    for(i, isprime) in enumerate(a):
+        if isprime:
+            # yield i  # Turn into generator
+            for n in range(i*i, limit, i):
+                a[n] = False
+    return a
+
+
+primes_sieve2(2000000)
+
+
