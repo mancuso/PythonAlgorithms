ef1fc593458ad8afb5605563aedf37e327933c43
U-CITYACCT\petman <petman@CSN3002755-16.civic.mississauga.ca>
2018-03-26 11:31:44 -0400

    Python 3 print statement

--- decorators.py
+++ decorators.py
@@ -10,7 +10,7 @@ def timeit(method):
             name = kwargs.get('log_name', method.__name__.upper())
             kwargs['log_time'][name] = int((te - ts) * 1000)
         else:
-            print '%r  %2.2f ms' % \
-                  (method.__name__, (te - ts) * 1000)
+            print ('%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000))
         return result
     return timed
